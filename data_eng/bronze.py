6977af265be3467b91e7fc5ef86b0c526f3f9c65
mieshiii <mieseliel.delacruz@gmail.com>
2024-04-20 23:59:46 +0800

    #9 - bronze sample

--- data_eng/bronze.py
+++ data_eng/bronze.py
@@ -1,7 +1,15 @@
 from ..data_scraper import scrape_website
 
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Bronze").getOrCreate()
+
 #start scraping
 url = 'https://www.lamudi.com.ph/house/buy/'
 
 data = scrape_website(url)
 
+df = spark.createDataFrame(data)
+df.printSchema()
+#sample abfss url
+df.write.parquet("abfss://parquet@deltaformatdemostorage.dfs.core.windows.net/employees")
