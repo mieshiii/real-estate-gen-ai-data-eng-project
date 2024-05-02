8a1c2cde2f9945b801371cd7add16a3dc8679a8b
mieshiii <mieseliel.delacruz@gmail.com>
2024-05-02 21:26:00 +0800

    #21 - refine price metrics

--- data_eng/gold.py
+++ data_eng/gold.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, avg, lit
+from pyspark.sql.functions import col, avg, lit, explode, count
 from pyspark.sql import Window
 
 spark = SparkSession.builder.appName("Gold").getOrCreate()
@@ -10,13 +10,17 @@ df = spark.read.parquet('abfss://parquet@deltaformatdemostorage.dfs.core.windows
 # Create a window specification for the running average calculation
 windowSpec = Window.orderBy("Date Ingested").rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
-df = df.withColumn("run_avg_price", avg(col("price")).over(windowSpec))
+df_price_metrics = df.withColumn("run_avg_price", avg(col("price")).over(windowSpec))
 
-df = df.withColumn("run_avg_land_size", avg(col("land_sizes")).over(windowSpec))
+df_price_metrics = df_price_metrics.withColumn("run_avg_land_size", avg(col("land_sizes")).over(windowSpec))
 
-df_final = df
+
+df_price_metrics = df_price_metrics.withColumn("price_per_land_size", col("Price") / col("Land Size"))
+
+
+df_price_metrics = df_price_metrics.withColumn("price_per_bldg_size", col("Price") / col("Building Size"))
+
+df_final = df_price_metrics
 
 #test silver parquet file and url
 df_final.write.parquet('abfss://parquet@deltaformatdemostorage.dfs.core.windows.net/employees_gold')
-
-
