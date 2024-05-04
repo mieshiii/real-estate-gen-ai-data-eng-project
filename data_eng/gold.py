a2565aa5cc6b9dbb2dd2abe6d5372679938dc2cc
mieshiii <mieseliel.delacruz@gmail.com>
2024-05-04 14:35:23 +0800

    #23 - add timestamps for processing

--- data_eng/gold.py
+++ data_eng/gold.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, avg, lit, explode, count
+from pyspark.sql.functions import col, avg, lit, explode, count, current_timestamp
 from pyspark.sql import Window
 
 spark = SparkSession.builder.appName("Gold").getOrCreate()
@@ -20,6 +20,8 @@ df_price_metrics = df_price_metrics.withColumn("price_per_land_size", col("Price
 
 df_price_metrics = df_price_metrics.withColumn("price_per_bldg_size", col("Price") / col("Building Size"))
 
+df_price_metrics = df_price_metrics.withColumn("time_of_processing", lit(current_timestamp()))
+
 df_final = df_price_metrics
 
 #test silver parquet file and url
@@ -28,8 +30,12 @@ df_final.write.parquet('abfss://parquet@deltaformatdemostorage.dfs.core.windows.
 df_exploded = df.select(col("category"), explode(col("subcategory")).alias("subcategory"))
 
 df_cat_counts = df.groupBy("category").agg(count(lit(1)).alias("count"))
+df_cat_counts = df_cat_counts.withColumn("time_of_processing", lit(current_timestamp()))
 
 # Compute the count of each subcategory
 df_subcat_counts = df_exploded.groupBy("cubcategory").agg(count(lit(1)).alias("count"))
+df_subcat_counts = df_subcat_counts.withColumn("time_of_processing", lit(current_timestamp()))
 
 df_cat_counts.write.parquet('abfss://parquet@deltaformatdemostorage.dfs.core.windows.net/employees_gold_cat')
+
+df_subcat_counts.write.parquet('abfss://parquet@deltaformatdemostorage.dfs.core.windows.net/employees_gold_subcat')
