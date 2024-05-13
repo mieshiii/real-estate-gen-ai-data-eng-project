9e7aafbc0c1942c8fb698d165bf052d10ae546d2
mieshiii <mieseliel.delacruz@gmail.com>
2024-05-13 21:59:19 +0800

    #29 - fix col names references

--- data_eng/gold.py
+++ data_eng/gold.py
@@ -8,17 +8,17 @@ spark = SparkSession.builder.appName("Gold").getOrCreate()
 df = spark.read.parquet('abfss://parquet@deltaformatdemostorage.dfs.core.windows.net/employees_silver')
 
 # Create a window specification for the running average calculation
-windowSpec = Window.orderBy("Date Ingested").rowsBetween(Window.unboundedPreceding, Window.currentRow)
+windowSpec = Window.orderBy("date_ingested").rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
 df_price_metrics = df.withColumn("run_avg_price", avg(col("price")).over(windowSpec))
 
 df_price_metrics = df_price_metrics.withColumn("run_avg_land_size", avg(col("land_sizes")).over(windowSpec))
 
 
-df_price_metrics = df_price_metrics.withColumn("price_per_land_size", col("Price") / col("Land Size"))
+df_price_metrics = df_price_metrics.withColumn("price_per_land_size", col("price") / col("land_size"))
 
 
-df_price_metrics = df_price_metrics.withColumn("price_per_bldg_size", col("Price") / col("Building Size"))
+df_price_metrics = df_price_metrics.withColumn("price_per_bldg_size", col("price") / col("building_size"))
 
 df_price_metrics = df_price_metrics.withColumn("time_of_processing", lit(current_timestamp()))
 
